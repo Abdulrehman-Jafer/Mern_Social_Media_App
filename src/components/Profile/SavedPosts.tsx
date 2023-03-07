101434cd0db0d57c5d6e0ab0bc18855e6e1a8b25
Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>
2023-03-07 22:17:47 +0500

    fixed saved posts null values

--- src/components/Profile/SavedPosts.tsx
+++ src/components/Profile/SavedPosts.tsx
@@ -6,33 +6,37 @@ import { UserContext } from '../../context/context'
 import { OfPost } from '../../types.'
 import ProfilePost from './ProfilePost'
 import Loader from '../../globles/Loader'
+import { toast } from "react-toastify"
 
 const SavedPosts = () => {
-    const { userData: { _id } } = useContext(UserContext)
+    const { userData: { _id, saved } } = useContext(UserContext)
     const [savedPosts, setSavedPosts] = useState<OfPost[]>([])
-    const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         const getSavedPosts = async () => {
             setLoading(true)
             let Url = getSavedPostUrl + "/" + _id
             await axios.get(Url).then(res => {
                 if (res.status == 200) {
+                    const data = res.data.savedPost.filter((value: OfPost) => value !== null)
                     setLoading(false)
-                    setSavedPosts(res.data.savedPost)
+                    setSavedPosts(data)
+                    return toast.success("Sucessfull")
                 }
             }).catch(err => {
                 setLoading(false)
                 console.log(err)
-        })
+                return toast.error("Something went wrong")
+            })
         }
         getSavedPosts()
     }, [])
-    const savedPostMap = savedPosts.length ?
+    const savedPostMap = savedPosts.length > 0 ?
         savedPosts.map(({ _id, comments, likes, image }) => <ProfilePost key={_id} id={_id} postImage={image} comments={comments} likes={likes} />) :
-        <h1 className='text-white'>You dont have any Posts yet</h1>
+        <h1 className='text-white'>You dont have any Saved Posts</h1>
     return (
         <main className='flex gap-[2rem] flex-wrap mt-[2rem] justify-center'>
-            {loading ? <Loader/> : savedPostMap}
+            {loading ? <Loader /> : savedPostMap}
         </main>
     )
 }
