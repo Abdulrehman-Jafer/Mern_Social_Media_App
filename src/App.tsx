0c81bc0a78fab7d3d383ad434e4cadb99a22d2fc
Abdulrehman-Jafer <abdulrehmanjaferwork01233@gmail.com>
2023-03-10 11:37:10 +0500

    added routes in seperate component

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,16 @@ import Login from './screens/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import useContextData from './hooks/useContextData'
+import NavigationRoutes from './components/navigation_routes/NavigationRoutes'
 
 
 const App = () => {
-  const { userData } = useContextData()
-  const navigate = useNavigate()
-  useEffect(() => {
-    if (!userData._id) {
-      return navigate("/login")
-    }
-    navigate("/")
-  }, [userData._id])
+ 
   return (
-    <main className='w-[100%] min-h-[100vh] nightSky'>
-      <Routes>
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
-      </Routes>
-      <ToastContainer />
-    </main>
+      <main className='w-[100%] min-h-[100vh] nightSky'>
+        <NavigationRoutes />
+        <ToastContainer />
+      </main>
 
   )
 }
---
+++ src/components/navigation_routes/NavigationRoutes.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { Route,useNavigate,Routes } from 'react-router-dom'
+import Profile from '../../screens/Profile'
+import SignUp from '../../screens/SignUp'
+import Home from "../../screens/Home"
+import Login from '../../screens/Login'
+import useContextData from '../../hooks/useContextData'
+
+
+const NavigationRoutes = () => {
+    const { userData } = useContextData()
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (!userData._id) {
+            return navigate("/login")
+        }
+        navigate("/")
+    }, [userData._id])
+
+    return (
+        <Routes>
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+        </Routes>
+    )
+}
+
+export default NavigationRoutes
