4e885be6f86c115fdce44f1ac4d5554628e677d8
Abdulrehman-Jafer <121712508+Abdulrehman-Jafer@users.noreply.github.com>
2023-06-22 20:13:50 +0500

    updated API's to connect vercel api

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,15 +1,15 @@
 // Users
-export const SignUpUrl = "https://spieser-api.onrender.com/api/user/create";
-export const LogInUrl = "https://spieser-api.onrender.com/api/user/login";
-export const renewUserUrl = "https://spieser-api.onrender.com/api/user/"
-export const getUserPostUrl = "https://spieser-api.onrender.com/api/user/posts"
-export const searchURl = "https://spieser-api.onrender.com/api/user/search"
+export const SignUpUrl = "https://spieser-backend.vercel.app/api/user/create";
+export const LogInUrl = "https://spieser-backend.vercel.app/api/user/login";
+export const renewUserUrl = "https://spieser-backend.vercel.app/api/user/"
+export const getUserPostUrl = "https://spieser-backend.vercel.app/api/user/posts"
+export const searchURl = "https://spieser-backend.vercel.app/api/user/search"
 // Posts
-export const createPostUrl = "https://spieser-api.onrender.com/api/post/create"
-export const getAllPostUrl = "https://spieser-api.onrender.com/api/post/"
-export const LikePostUrl = "https://spieser-api.onrender.com/api/post/likepost"
-export const AddCommentUrl = "https://spieser-api.onrender.com/api/post/addcomment"
-export const GetAllCommenstUrl = "https://spieser-api.onrender.com/api/post/comments"
-export const deletePostUrl = "https://spieser-api.onrender.com/api/post/deletepost"
-export const savePostUrl = "https://spieser-api.onrender.com/api/post/savepost"
-export const getSavedPostUrl = "https://spieser-api.onrender.com/api/post/getsavedpost"
+export const createPostUrl = "https://spieser-backend.vercel.app/api/post/create"
+export const getAllPostUrl = "https://spieser-backend.vercel.app/api/post/"
+export const LikePostUrl = "https://spieser-backend.vercel.app/api/post/likepost"
+export const AddCommentUrl = "https://spieser-backend.vercel.app/api/post/addcomment"
+export const GetAllCommenstUrl = "https://spieser-backend.vercel.app/api/post/comments"
+export const deletePostUrl = "https://spieser-backend.vercel.app/api/post/deletepost"
+export const savePostUrl = "https://spieser-backend.vercel.app/api/post/savepost"
+export const getSavedPostUrl = "https://spieser-backend.vercel.app/api/post/getsavedpost"
